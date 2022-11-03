b82ea22d2e093bf0835999c71d9e0095f0da0122
daohoangson123 <daohoangson2109@gmail.com>
2022-11-03 15:57:10 +0700

    lol

---
+++ kmin-lib.js
@@ -0,0 +1,13 @@
+function sleep(milliseconds) {
+    var start = new Date().getTime();
+    while (new Date().getTime() - start < milliseconds) { }
+  }
+  
+  function getRandomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  // export {sleep, getRandomInteger};
+  exports.getRandomInteger = getRandomInteger;
+  exports.sleep = sleep;
+  
+  
--- sub.js
+++ sub.js
@@ -1,17 +1,11 @@
-function sleep(milliseconds) {
-    var start = new Date().getTime();
-    while (new Date().getTime() - start < milliseconds);
-}
+let readline = require('readline-sync');
+let kmin = require('./kmin-lib');
 
-function getRandomInteger(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) ) + min;
-}
-
-//
-function taoSoBiMat() {
+// Tạo mảng số bí mật, mỗi số đc random trong đoạn [min, max]
+function taoSoBiMat(min, max) {
     let soBiMat = [];
     for (let i = 1; i <= 5; i++) {
-        let x = getRandomInteger(0, 9);
+        let x = kmin.getRandomInteger(min, max);
         soBiMat.push(x);
     }
     return soBiMat;
@@ -20,38 +14,59 @@ function taoSoBiMat() {
 function laySoNguoiDung() {
     let soNguoiDung = [];
 
-    // for
-
+    for (let i = 1; i <= 5; i++) {
+        let x = readline.question(`Nhap so thu ${i}: `);
+        soNguoiDung.push(x);
+    }
     return soNguoiDung;
 }
-
-laySoNguoiDung();
-
+// Hàm kiểm tra mảng số bí mật và mảng số người dùng
 function kiemTra(soBiMat, soNguoiDung) {
-
+    for (let i = 0; i < soBiMat.length; i++) {
+        if (soBiMat[i] != soNguoiDung[i])
+            return false;
+    }
+    return true;
 }
-function main() {
-    // Tạo 5 số ngẫu nhiên và lưu vào mảng
-    let soBiMat = taoSoBiMat();
-    console.log(soBiMat);
 
+function chayManChoi(capDo) {
+    // Tạo 5 số ngẫu nhiên và lưu vào mảng
+    let soBiMat = taoSoBiMat(10 ** (capDo - 1), 10 ** capDo - 1);
 
     // Hiển thị 5 con số ngẫu nhiên
     console.log(soBiMat);
 
     // Sau 1 giây thì xóa 5 số đó đi
-    // sleep(1000);
+    kmin.sleep(1000 * capDo);
     // console.clear();
 
     // Cho nguoi dung nhập
     let soNguoiDung = laySoNguoiDung();
 
+    return [soBiMat, soNguoiDung];
+}
 
-    // Xét đúng sai
-    if (kiemTra(soBiMat, soNguoiDung) == true)
-        console.log("Ban da doan dung");
-    else
-        console.log("Game over");
+function main() {
+    for (let i = 1; i <= 5; i++) {
+        let data = chayManChoi(i);
+        let soBiMat = data[0];
+        let soNguoiDung = data[1];
+
+        console.log(soBiMat);
+        console.log(soNguoiDung);
+
+        // Xét đúng sai
+        if (kiemTra(soBiMat, soNguoiDung) == true) {
+            console.log("Ban da doan dung");
+            // let data = chayManChoi(2);
+            // let soBiMat = data[0];
+            // let soNguoiDung = data[1];
+        }
+        else {
+            console.log("Game over");
+            break;
+        }
+    }
 }
 
 main();
